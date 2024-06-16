429ae9c7879d89e79a6e4c224e48e7c4d20959b2
feyzaozcini <feyza_ozcini@hotmail.com>
2024-06-16 23:04:38 +0300

    Basket Service Edited

--- basketservice/src/main/java/com/turkcell/basketservice/services/concretes/BasketServiceImpl.java
+++ basketservice/src/main/java/com/turkcell/basketservice/services/concretes/BasketServiceImpl.java
@@ -1,6 +1,37 @@
 package com.turkcell.basketservice.services.concretes;
 
 import com.turkcell.basketservice.services.abstracts.BasketService;
+import com.turkcell.basketservice.services.dtos.requests.BasketAddItemRequest;
+import com.turkcell.basketservice.services.dtos.requests.BasketCreateRequest;
+import com.turkcell.basketservice.services.dtos.requests.BasketDeleteItemRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
+@Service
+@RequiredArgsConstructor
 public class BasketServiceImpl implements BasketService {
+    @Override
+    public void createCart(BasketCreateRequest request) {
+
+    }
+
+    @Override
+    public void addItemToCart(BasketAddItemRequest request) {
+
+    }
+
+    @Override
+    public void deleteItemFromCart(BasketDeleteItemRequest request) {
+
+    }
+
+    @Override
+    public void clearCartById(int cartId) {
+
+    }
+
+    @Override
+    public void deleteCartById(int cartId) {
+
+    }
 }
