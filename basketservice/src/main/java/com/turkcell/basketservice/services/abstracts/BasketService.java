f227577381bfdef62001d7f3bb5d6074d661bdf2
feyzaozcini <feyza_ozcini@hotmail.com>
2024-06-16 23:04:57 +0300

    Basket Service Edited

--- basketservice/src/main/java/com/turkcell/basketservice/services/abstracts/BasketService.java
+++ basketservice/src/main/java/com/turkcell/basketservice/services/abstracts/BasketService.java
@@ -1,4 +1,13 @@
 package com.turkcell.basketservice.services.abstracts;
 
+import com.turkcell.basketservice.services.dtos.requests.BasketAddItemRequest;
+import com.turkcell.basketservice.services.dtos.requests.BasketCreateRequest;
+import com.turkcell.basketservice.services.dtos.requests.BasketDeleteItemRequest;
+
 public interface BasketService {
+    void createCart(BasketCreateRequest request);
+    void addItemToCart(BasketAddItemRequest request);
+    void deleteItemFromCart(BasketDeleteItemRequest request);
+    void clearCartById(int cartId);
+    void deleteCartById(int cartId);
 }
