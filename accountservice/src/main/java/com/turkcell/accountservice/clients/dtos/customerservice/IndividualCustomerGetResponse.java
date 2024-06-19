03f97405f8ef971aecbba28ca42f1a38c1297a21
burakonurerten <burakonurerten@gmail.com>
2024-06-19 12:46:07 +0300

    change customer dto from accountservice

--- accountservice/src/main/java/com/turkcell/accountservice/clients/dtos/customerservice/IndividualCustomerGetResponse.java
+++ accountservice/src/main/java/com/turkcell/accountservice/clients/dtos/customerservice/IndividualCustomerGetResponse.java
@@ -5,7 +5,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
+
 @Getter
 @Setter
 @AllArgsConstructor
@@ -15,20 +16,20 @@ public class IndividualCustomerGetResponse {
     private String firstName;
     private String secondName;
     private String lastName;
-    private Long nationalityId;
-    private Long accountNumber;
-    private Long gsmNumber;
-    private Long orderNumber;
+    private String nationalityId;
+    private String accountNumber;
+    private String gsmNumber;
+    private String orderNumber;
     private String motherName;
     private String fatherName;
     private Gender gender;
-    private LocalDateTime birthDate;
+    private LocalDate birthDate;
     private int defaultAddressId;
+
     @Getter
-    public enum Gender{
+    public enum Gender {
         MALE,
         FEMALE,
         NOT_SPECIFIED
     }
-
 }
